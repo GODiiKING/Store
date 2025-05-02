262527f5633c74183754c5a696e997358e2e064b
Dinesh van Putten <dinozblue@live.com>
2025-05-02 15:17:38 +0200

    inserting brands

--- admin_area/insert_brands.php
+++ admin_area/insert_brands.php
@@ -1,12 +1,34 @@
-<form action="" methods="post" class="mb-2">
+<?php 
+
+include('../includes/connect.php');
+if(isset($_POST['insert_brand'])){
+    $brand_title = mysqli_real_escape_string($con, $_POST['brand_title']); // Prevent SQL injection
+
+    // Select data from database
+    $select_query = "SELECT * FROM `brands` WHERE brand_title='$brand_title'";
+    $result_select = mysqli_query($con, $select_query);
+    $number = mysqli_num_rows($result_select);
+    if($number > 0){
+        echo "<script>alert('This brand is already present in the database')</script>";
+    } else {
+        $insert_query = "INSERT INTO `brands` (brand_title) VALUES ('$brand_title')";
+        $result = mysqli_query($con, $insert_query);
+        if($result){
+            echo "<script>alert('Brand Inserted Successfully')</script>";
+        } else {
+            echo "<script>alert('Failed to insert brand')</script>";
+        }
+    }
+}
+?>
+
+<form action="" method="post" class="mb-2"> <!-- Fixed 'method' attribute -->
 <div class="input-group w-90 mb-2">
   <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
-  <input type="text" class="form-control" name="cart_title" placeholder="Insert Brands" aria-label="Brands" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" name="brand_title" placeholder="Insert Brands" aria-label="Brands" aria-describedby="basic-addon1">
 </div>
 
 <div class="input-group w-10 mb-2 m-auto">
-
-  <!-- <input type="submit" class="form-control bg-info" name="insert_cat" value="Insert Categories"> -->
-   <button class="bg-info p-2 my-3 border-0">Insert Brands</button>
+  <input type="submit" class="bg-info border-0 p-2 my-3" name="insert_brand" value="Insert Brands">
 </div>
 </form>
