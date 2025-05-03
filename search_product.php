e2654db5c454f16fcdfa79a57d34a5661b30d4c8
Dinesh van Putten <dinozblue@live.com>
2025-05-03 09:17:11 +0200

    Searching products

--- search_product.php
+++ search_product.php
@@ -57,7 +57,7 @@ include('functions/common_function.php');
           <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
       </ul>
-      <form class="d-flex" action="search_product.php" method="get">
+      <form class="d-flex" action="" method="get">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_data">
          <input type="submit" value="Search" class="btn btn-outline-light" name="search_data_product">
       </form>
@@ -93,7 +93,7 @@ include('functions/common_function.php');
       <!-- fetching products -->
       <?php 
       // calling function
-      getproducts();
+      search_product();
       get_unique_categories();
       get_unique_brands();      
       ?>
