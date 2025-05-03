7369fb4bf57c290f88a48b91bce86b3288141cc1
Dinesh van Putten <dinozblue@live.com>
2025-05-03 06:49:16 +0200

    Using Functions

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!-- connect to file -->
 <?php 
 include('includes/connect.php');
+include('functions/common_function.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -91,31 +92,8 @@ include('includes/connect.php');
       <div class="row">
       <!-- fetching products -->
       <?php 
-      $select_query="Select * from `products` order by rand() limit 0,9";
-      $result_query=mysqli_query($con, $select_query);
-      // $row=mysqli_fetch_assoc($result_query);
-        // echo $row['product_title'];
-        while($row=mysqli_fetch_assoc($result_query)){
-          $product_id=$row['product_id'];
-          $product_title=$row['product_title'];
-          $product_description=$row['product_description'];
-          $product_image1=$row['product_image1'];
-          $product_image2=$row['product_image2'];
-          $product_image3=$row['product_image3'];
-          $product_price=$row['product_price'];
-          $category_id=$row['category_id'];
-          $brand_id=$row['brand_id'];
-          echo "<div class='col-md-4 mb-2'><div class='card'>
-                <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='$product_title'>
-                <div class='card-body'>
-                  <h5 class='card-title'>$product_title</h5>
-                  <p class='card-text'>$product_description</p>
-                  <a href='#' class='btn btn-info'>Add to cart</a>
-                  <a href='#' class='btn btn-secondary'>View more</a>
-                </div>
-        </div>
-</div>";
-}
+      // calling function
+      getproducts();
       ?>
 <!-- row end -->
 </div>
@@ -132,17 +110,7 @@ include('includes/connect.php');
         <li class="nav-item bg-info">
           <a href="#" class="nav-link text-light"><h4>Delivery Brands</h4></a>
           <?php 
-          $select_brands="select * from `brands`";
-          $result_brands=mysqli_query($con, $select_brands);
-          // $row_data=mysqli_fetch_assoc($result_brands);
-          // echo $row_data['brand_title'];
-          while($row_data=mysqli_fetch_assoc($result_brands)){
-            $brand_title=$row_data['brand_title'];
-            $brand_id=$row_data['brand_id'];
-            echo "<li class='nav-item'>
-            <a href='index.php?brand=$brand_id' class='nav-link text-light'>$brand_title</a>
-            </li>";
-          }
+          getbrands()
           ?>
         </li>
 
@@ -157,17 +125,7 @@ include('includes/connect.php');
         </li>
 
         <?php 
-          $select_categories="select * from `categories`";
-          $result_categories=mysqli_query($con, $select_categories);
-          // $row_data=mysqli_fetch_assoc($result_brands);
-          // echo $row_data['brand_title'];
-          while($row_data=mysqli_fetch_assoc($result_categories)){
-            $category_title=$row_data['category_title'];
-            $category_id=$row_data['category_id'];
-            echo "<li class='nav-item'>
-            <a href='index.php?category=$category_id' class='nav-link text-light'>$category_title</a>
-            </li>";
-          }
+          getcategories()
           ?>
        </ul>
 
