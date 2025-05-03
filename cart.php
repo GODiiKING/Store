a62203de300eedee7d6d3bd551a22c7e2edcb861
Dinesh van Putten <dinozblue@live.com>
2025-05-03 15:27:11 +0200

    Removing items from cart

--- cart.php
+++ cart.php
@@ -125,7 +125,7 @@ cart()
                        ?>
                         <tr>
                             <td><?php echo $product_title ?></td>
-                            <td><img src="./images/<?php echo $product_image1 ?>" alt="" class="cart_img"></td>
+                            <td><img src="./admin_area/product_images/<?php echo $product_image1 ?>" alt="" class="cart_img"></td>
                             <td><input type="text" name="qty" id="" class="form-input w-50"></td>
                             <?php  
                             $get_ip_add=getIPAddress();
@@ -137,11 +137,12 @@ cart()
                             }
                             ?>
                             <td><?php echo $price_table ?>/-</td>
-                            <td><input type="checkbox"></td>
+                            <td><input type="checkbox" name="removeitem[]" value="<?php echo $product_id  ?>"></td>
                             <td>
                               <!-- <button class="bg-info px-3 py-2 border-0 mx-3">Update</button> -->
                                <input type="submit" value="Update Cart" name="update_cart" class="bg-info px-3 py-2 border-0 mx-3">
-                              <button class="bg-info px-3 py-2 border-0 mx-3">Remove</button>
+                              <!-- <button class="bg-info px-3 py-2 border-0 mx-3">Remove</button> -->
+                              <input type="submit" value="Remove Cart" name="remove_cart" class="bg-info px-3 py-2 border-0 mx-3">
                             </td>
 
                         </tr>
@@ -160,6 +161,26 @@ cart()
             </div>
         </div>
         </form>
+        <!-- function to remove item from cart -->
+        <?php 
+        function remove_cart_item(){
+          global $con;
+          if(isset($_POST['remove_cart'])){
+            foreach($_POST['removeitem'] as $remove_id){
+              echo $remove_id;
+              $delete_query="Delete from `cart_details` where product_id='$remove_id'";
+              $run_delete=mysqli_query($con, $delete_query);
+              if($run_delete){
+                echo "<script>window.open('cart.php','_self')</script>";
+              }
+            }
+          }
+        }
+        echo $remove_item=remove_cart_item();
+        ?>
+
+
+
          <!-- footer -->
           <?php  
           include('./includes/footer.php');
