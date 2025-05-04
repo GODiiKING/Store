95660f10037fe43a5e07fc2735f513ad00be6b81
Dinesh van Putten <dinozblue@live.com>
2025-05-04 13:31:41 +0200

    User Redirection (cart.php) link issues

--- users_area/payment.php
+++ users_area/payment.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Store - Payment</title>
 </head>
 <body>
-    <h1>2</h1>
+    <h1>Payment</h1>
 </body>
 </html>
